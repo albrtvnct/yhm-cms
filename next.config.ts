51492dd2d177d85a5fb65d29a987ee6ab8350521
Albert Vincent <albrtvnct@gmail.com>
2026-05-29 15:25:22 +0700

    Configure serverActions with allowed origin
    Added serverActions configuration with allowed origins.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  serverActions: {
+    allowedOrigins: [
+      "localhost:3000",
+      "cmsyhm.vercel.app",
+      "yeshproduction.my.id",
+      "www.yeshproduction.my.id",
+    ],
+  },
 };
 
 export default nextConfig;
