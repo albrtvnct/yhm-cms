d32d5d85d88263e92eedf1a7f341de9f7e5aae38
Albert Vincent <albrtvnct@gmail.com>
2026-05-29 15:45:31 +0700

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  serverActions: {
-    allowedOrigins: [
-      "localhost:3000",
-      "cmsyhm.vercel.app",
-      "yeshproduction.my.id",
-      "www.yeshproduction.my.id",
-      "https://yeshproduction.my.id",
-      "https://www.yeshproduction.my.id",
-    ],
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000",
+        "cmsyhm.vercel.app",
+        "yeshproduction.my.id",
+        "www.yeshproduction.my.id",
+        "https://yeshproduction.my.id",
+        "https://www.yeshproduction.my.id",
+      ],
+    },
   },
 };
 
